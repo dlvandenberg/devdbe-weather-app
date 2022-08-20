a7976481f5b24f07102d051f6308fe1cce36bf32
Dennis van den Berg <dennis.vandenberg@nedap.com>
2022-08-20 19:03:48 +0200

    New xcode project.

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Dennis van den Berg on 20/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
